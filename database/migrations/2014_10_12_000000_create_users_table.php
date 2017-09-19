8f099667b8ee663d9ea3cf0593e4060635869799
jerry5 <jerry@suitmedia.com>
2017-09-19 16:00:22 +0700

    modify migration file for table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,11 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique()->nullable();
             $table->string('password')->nullable();
+            $table->enum('agama', ['kristen', 'katolik', 'islam', 'budha', 'hindu', 'konghucu']);
+            $table->string('suku')->nullable();
+            $table->string('pendidikan_tertinggi')->nullable();
+            $table->string('pekerjaan')->nullable();
+            $table->enum('pendapatan', ['< Rp. 5.000.000,-', 'Rp. 5.000.000,-  s/d  Rp. 10.000.000,-', 'Rp. 10.000.000,-  s/d  Rp. 20.000.000,-', '>  Rp. 20.000.000,-']);
             $table->string('image')->nullable();
             $table->date('date_of_birth')->nullable();
             $table->enum('gender', ['laki-laki', 'perempuan']);
